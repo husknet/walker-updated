dd64e76aa7d5356ff146404dc7cad371582ff9b1
husknet <127146284+husknet@users.noreply.github.com>
2024-12-10 15:52:29 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.abruis.io';
+        window.location.href = 'https://wh.abruis.io';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
